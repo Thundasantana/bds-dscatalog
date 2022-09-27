7e076f5ce60492080c9e5dc341d35713e4219b60
Ednilson Santana <ednilson.sdas@outlook.com>
2022-09-27 13:45:05 -0300

    formatPrice

--- frontweb/src/components/ProductPrice/index.tsx
+++ frontweb/src/components/ProductPrice/index.tsx
@@ -1,5 +1,5 @@
+import { formatPrice } from 'util/formatters';
 import './styles.css';
-
 
 type Props = {
     price: number;
@@ -10,7 +10,7 @@ const ProductPrice = ( { price } : Props) => {
     return (
         <div className="product-price-container">
             <span>R$</span>
-            <h3>{price}</h3>
+            <h3>{formatPrice(price)}</h3>
         </div>
     );
 }
