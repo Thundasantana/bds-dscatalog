4400a120fcfe956573e7c3a5b5c0c4ee02f38ddc
Ednilson Santana <ednilson.sdas@outlook.com>
2022-11-28 21:14:12 -0300

    Redirecting to login when 401 or 403

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { config } from 'process';
 import qs from 'qs';
+import history from './history';
 
 type LoginResponse = {
     access_token: string;
@@ -56,3 +57,24 @@ export const getAuthData = () => {
     const str = localStorage.getItem(tokenKey) ?? '{}';
     return JSON.parse(str) as LoginResponse;
 }
+
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+    //
+    return config;
+  }, function (error) {
+    //
+    return Promise.reject(error);
+  });
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+    //
+    return response;
+  }, function (error) {
+    if (error.response.status === 401 || error.response.status === 403) {
+        history.push('/admin/auth');
+    }
+    //
+    return Promise.reject(error);
+  });
